f3bb11bcf9727ee522406dd1cbed5105c834bc0b
sovaonetrick <iankim8923@gmail.com>
2023-10-05 04:13:32 -0700

    I hope I did this right

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -1,17 +1,23 @@
 package u3pp;
 
 /**
- * YOUR COMMENTs HERE
- * 
- * @author //YOUR NAME HERE
+ * I could be on Summoner's Rift right now slaying Baron Nashor.
+ * This code should return true or false as to whether a given date is a leap year.
+ * @ian 
  *
  */
 
 public class LeapYear {
 	/**
-	 * YOUR COMMENTS HERE
+	 * This code first checks to ensure that the given date is past 1582. It then checks to see whether the given date is divisible by four. 
+	 * It then checks to make sure that the date is not divisible by 100, or if it is, it much also be divisible by 400.
 	 */
 	public static boolean isLeapYear(int year) {
-		/* YOUR CODE HERE */
+		if ((year >= 1582 && year % 4 == 0)&&(year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -3,25 +3,37 @@ package u3pp;
 import java.util.Scanner;
 
 /**
- * YOUR COMMENTS HERE
+ * This code takes the input for the first time, runs it into a while loop, and then runs it through isLeapYear to test whether it is or isn't a
+ * leap year. Once the result has been shown, the next input starts at the beginning of the while loop rather than having to start over.
  * 
- * @author YOUR NAME HERE
+ * @ian
  */
 
 class Main {
 
 	public static void main(String[] args) {
 
-		//YOUR CODE HERE
+		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter a year (or enter STOP to end): ");
+		System.out.println("Enter a year (or enter STOP to end): ");
 		String input = sc.nextLine();
 
 		while (!input.toUpperCase().equals("STOP")) {
-			/* YOUR CODE HERE */
-			System.out.print("Enter a year (or enter STOP to end): ");
-			//YOUR CODE HERE
+
+		int pickles = Integer.parseInt(input);
+		boolean tomatoes = LeapYear.isLeapYear(pickles);
+
+		if (tomatoes == true) {
+			System.out.println(input + " is a leap year");
 		}
-		//YOUR CODE HERE
+		else {
+			System.out.println(input + " is not a leap year");
+		}
+
+		System.out.println("Enter a year (or enter STOP to end): ");
+		input = sc.nextLine();
+		}
+
+		sc.close();
 	}
 }
